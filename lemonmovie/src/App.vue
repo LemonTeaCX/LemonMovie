72e5fb02ddcf1f1cfbab5e67e0839e9d91201962
lemon <1406428663@qq.com>
2017-03-09 18:08:57 +0800

    引入阿里字体图标并书写tab导航栏样式

--- lemonmovie/src/App.vue
+++ lemonmovie/src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <div class="content">
+      <router-view></router-view>
+    </div>
     <div class="tab">
       <div class="tab-item">
-        <router-link to="/movie">电影</router-link>
+        <router-link to="/movie">
+          <i class="iconfont">&#xe611;</i>
+          <span class="text">电影</span>
+        </router-link>
       </div>
       <div class="tab-item">
-        <router-link to="/discovery">发现</router-link>
+        <router-link to="/discovery">
+          <i class="iconfont">&#xe60e;</i>
+          <span class="text">发现</span>
+        </router-link>
       </div>
       <div class="tab-item">
-        <router-link to="/user">我的</router-link>
+        <router-link to="/user">
+          <i class="iconfont">&#xe609;</i>
+          <span class="text">我的</span>
+        </router-link>
       </div>
     </div>
   </div>
@@ -21,21 +32,40 @@ export default {
 </script>
 
 <style lang="less" scoped>
+@import "./common/less/index";
 #app {
+  .content {
+    padding-bottom: 50px;
+  }
   .tab {
     position: fixed;
     left: 0;
     bottom: 0;
     width: 100%;
     height: 50px;
-    background: #f5f5f5;
+    background: #eee;
     border-top: 1px solid #e3e3e3;
     display: flex;
     text-align: center;
-    line-height: 50px;
     font-size: 12px;
     .tab-item {
       flex: 1;
+      .iconfont {
+        display: block;
+        margin: 5px 0 3px;
+        font-size: 24px;
+      }
+      .text {
+        display: block;
+      }
+      .active {
+        .iconfont {
+          color: @lmgreen;
+        }
+        .text {
+          color: @lmgreen;
+        }
+      }
     }
   }
 }
