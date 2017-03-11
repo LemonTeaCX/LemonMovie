472be264a895315f0310c2943e45243c983f7df2
lemon <1406428663@qq.com>
2017-03-11 21:01:59 +0800

    书写部分movie组件

--- lemonmovie/src/components/movie/movie.vue
+++ lemonmovie/src/components/movie/movie.vue
@@ -1,13 +1,36 @@
 <template>
   <div class="movie">
-    <h1>this is a movie</h1>
+    <div class="header">
+      <div class="title">
+        <div class="city">广州</div>
+        <div class="tab">
+          <ul>
+            <li class="tab-item" v-for="item in tabItem">{{item}}</li>
+          </ul>
+        </div>
+      </div>
+      <div class="search">
+        <input type="text" placeholder="找影视剧、影人、影院">
+      </div>
+    </div>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
-export default {}
+let tabItem = ['热映', '待映', '找片'];
+export default {
+  data() {
+    return {
+      tabItem
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
+.movie {
+  width: 100%;
+}
 
 </style>
