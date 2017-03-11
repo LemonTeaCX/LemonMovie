142e22f2379afdfaa6c324a53a5c16622941d97d
lemon <1406428663@qq.com>
2017-03-11 20:52:02 +0800

    设置eslint规则忽略以分号结尾和函数名后面必须加空格的错误检查

--- lemonmovie/.eslintrc.js
+++ lemonmovie/.eslintrc.js
@@ -22,6 +22,10 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // space-before-function-paren
+    'space-before-function-paren': 0,
+    // semi
+    'semi': 0
   }
 }
