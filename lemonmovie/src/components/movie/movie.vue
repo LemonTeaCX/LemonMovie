7c3a8b1e2487137344e1517ebd3e32e59324114d
lemon <1406428663@qq.com>
2017-03-12 22:30:08 +0800

    引入hot、wait、find组件

--- lemonmovie/src/components/movie/movie.vue
+++ lemonmovie/src/components/movie/movie.vue
@@ -4,33 +4,113 @@
       <div class="title">
         <div class="city">广州</div>
         <div class="tab">
-          <ul>
-            <li class="tab-item" v-for="item in tabItem">{{item}}</li>
+          <ul class="tab-box">
+            <li class="tab-item" @click="changeCur(index)" :class="{'cur': item.active}" v-for="(item, index) in tabItems">
+              <router-link :to="item.linkto">{{item.text}}</router-link>
+            </li>
           </ul>
         </div>
       </div>
-      <div class="search">
-        <input type="text" placeholder="找影视剧、影人、影院">
-      </div>
+      <search></search>
     </div>
-    <div class="content"></div>
+    <div class="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-let tabItem = ['热映', '待映', '找片'];
+// import router from '../../router'
+import search from 'components/search/search'
+let tabItems = [
+  { text: '热映', linkto: '/movie/hot', active: true },
+  { text: '待映', linkto: '/movie/wait', active: false },
+  { text: '找片', linkto: '/movie/find', active: false }
+]
 export default {
+  created() {
+    // console.log(router.currentRoute)
+    // console.log(router)
+    // let lemon = '/movie/wait';
+    // router.push(lemon);
+    // console.log(0)
+    /* this.$on('cur-href', (curHref) => {
+      console.log('come in')
+      console.log(curHref)
+      router.push(curHref);
+    }) */
+    // this.$parent.$emit('cur-href', router.currentRoute.path)
+    // let curHref = this.myUrl;
+    // router.push(curHref);
+    console.log(this.$route)
+  },
   data() {
     return {
-      tabItem
+      tabItems
+    }
+  },
+  components: {
+    search
+  },
+  methods: {
+    changeCur(index) {
+      this.tabItems.forEach(tabItem => {
+        tabItem.active = false;
+      })
+      this.tabItems[index].active = true;
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+@import "../../common/less/index";
 .movie {
   width: 100%;
+  overflow: hidden;
+  .header {
+    width: 100%;
+    background: @lmgreen;
+    .title {
+      width: 100%;
+      height: 60px;
+      color: #fff;
+      position: relative;
+      overflow: hidden;
+      .city {
+        position: absolute;
+        left: 30px;
+        top: 25px;
+      }
+      .tab {
+        width: 180px;
+        height: 34px;
+        margin: 15px auto 0;
+        padding: 2px;
+        background: #77AD65;
+        border-radius: 5px;
+        line-height: 34px;
+        text-align: center;
+        font-size: 14px;
+        .tab-box {
+          display: flex;
+          .tab-item {
+            flex: 1;
+            border-radius: 5px;
+            &.cur {
+              background: #fff;
+              a {
+                color: #666;
+              }
+            }
+            a {
+              color: #fff;
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 </style>
