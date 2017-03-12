0622e72721c021935497cd2070c0dd28fb654f40
lemon <1406428663@qq.com>
2017-03-12 22:25:18 +0800

    添加find、hot、wait组件

---
+++ lemonmovie/src/components/movie/find.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="find">
+    <h1>this is find</h1>
+  </div>
+</template>
+
+<script>
+// import router from '../../router'
+export default {
+  created() {
+    console.log(3)
+    // this.$parent.$emit('cur-href', router.currentRoute.path)
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
