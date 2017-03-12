8407489ecbc9aac43e2ae8b402dd7df446b2bef6
lemon <1406428663@qq.com>
2017-03-12 22:30:39 +0800

    配置movie下子级路由

--- lemonmovie/src/router/index.js
+++ lemonmovie/src/router/index.js
@@ -3,13 +3,25 @@ import Router from 'vue-router'
 import movie from 'components/movie/movie'
 import discovery from 'components/discovery/discovery'
 import user from 'components/user/user'
+import hot from 'components/movie/hot'
+import wait from 'components/movie/wait'
+import find from 'components/movie/find'
 
 Vue.use(Router)
 
 const routes = [
-  { path: '/movie', component: movie },
-  { path: '/discovery', component: discovery },
-  { path: '/user', component: user }
+  {
+    path: '/movie',
+    component: movie,
+    name: 'movie',
+    children: [
+      { path: 'hot', component: hot },
+      { path: 'wait', component: wait },
+      { path: 'find', component: find }
+    ]
+  },
+  { path: '/discovery', component: discovery, name: 'discovery' },
+  { path: '/user', component: user, name: 'user' }
 ]
 
 export default new Router({
