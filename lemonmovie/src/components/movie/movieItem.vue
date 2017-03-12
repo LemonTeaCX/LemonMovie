23f8ee267360c18e0b923d91b9ac3ba05632a708
lemon <1406428663@qq.com>
2017-03-12 22:26:43 +0800

    添加movieItem组件

---
+++ lemonmovie/src/components/movie/movieItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="movie-item">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    hot: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
