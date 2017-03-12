1275b490f6ce5154c4af58e625bfc50c3b3e0ce3
lemon <1406428663@qq.com>
2017-03-12 22:25:38 +0800

    添加find、hot、wait组件

--- lemonmovie/src/components/movie/hot.vue
+++ lemonmovie/src/components/movie/hot.vue
@@ -1,13 +1,44 @@
 <template>
   <div class="hot">
-    <h1>this is hot</h1>
+    <div class="movie">
+      <ul class="movie-list">
+        <li class="movie-item">
+          <movie-item :hot="movieHot.hot"></movie-item>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+// import router from '../../router'
+import movieItem from 'components/movie/movieItem.vue'
+export default {
+  data() {
+    return {
+      movieHot: {}
+    }
+  },
+  created() {
+    // console.log(1)
+    // this.$parent.$emit('cur-href', router.currentRoute.path)
+    this.axios.get('/movie/movieHot').then(response => {
+      if (response.statusText === 'OK') {
+        this.movieHot = response.data.data.data
+        console.log(this.movieHot)
+      }
+    }).catch(error => {
+      console.log(error)
+    })
+  },
+  components: {
+    'movie-item': movieItem
+  }
+}
 </script>
 
 <style lang="less" scoped>
-
+.hot {
+  width: 100%;
+}
 </style>
