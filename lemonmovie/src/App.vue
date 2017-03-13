217fcfccf97b025470701e2d4a745866915c1408
lemon <1406428663@qq.com>
2017-03-13 22:52:39 +0800

    添加路由切换过渡效果

--- lemonmovie/src/App.vue
+++ lemonmovie/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
     <div class="content">
-      <router-view></router-view>
+      <transition :name="slide">
+        <router-view></router-view>
+      </transition>
     </div>
     <div class="tab">
-      <div class="tab-item" @click="changeActive(index)" v-for="(item, index) in navItems">
-        <router-link :to="item.linkto" :class="{'active': item.active}">
+      <div class="tab-item" @click="changeActive(index)" v-for="(item, index) in navItems" :class="{'active': item.active}">
+        <router-link :to="item.linkto">
           <span class="iconfont" v-html="item.icon"></span>
           <span class="text">{{item.text}}</span>
         </router-link>
@@ -16,14 +18,32 @@
 
 <script>
 let navItems = [
-  { text: '电影', linkto: '/movie', active: true, icon: '&#xe611;' },
-  { text: '发现', linkto: '/discovery', active: false, icon: '&#xe60e;' },
-  { text: '我的', linkto: '/user', active: false, icon: '&#xe609;' }
+  { text: '电影', name: 'movie', linkto: '/movie', active: true, icon: '&#xe611;' },
+  { text: '发现', name: 'discovery', linkto: '/discovery', active: false, icon: '&#xe60e;' },
+  { text: '我的', name: 'user', linkto: '/user', active: false, icon: '&#xe609;' }
 ]
 export default {
   data() {
     return {
-      navItems
+      navItems,
+      slide: ''
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      let fromIndex = -1;
+      let toIndex = -1;
+      this.navItems.forEach((item, index) => {
+        if (item.name === from.name) {
+          fromIndex = index;
+          return;
+        }
+        if (item.name === to.name) {
+          toIndex = index;
+          return;
+        }
+      })
+      this.slide = fromIndex < toIndex ? 'slide-left' : 'slide-right';
     }
   },
   methods: {
@@ -39,6 +59,18 @@ export default {
 
 <style lang="less" scoped>
 @import "./common/less/index";
+.slide-right-enter-active, {
+  transition: all .5s;
+}
+.slide-right-enter, {
+  transform: translateX(-100%);
+}
+.slide-left-enter-active {
+  transition: all .5s;
+}
+.slide-left-enter {
+  transform: translateX(100%);
+}
 #app {
   .content {
     padding-bottom: 50px;
@@ -60,11 +92,13 @@ export default {
         display: block;
         margin: 5px 0 3px;
         font-size: 20px;
+        transition: all 0.3s;
       }
       .text {
         display: block;
+        transition: all 0.3s;
       }
-      .active {
+      &.active {
         .iconfont {
           color: @lmgreen;
         }
