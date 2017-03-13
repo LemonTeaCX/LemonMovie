84482696564a72f3b2d25c1a6134ebd479f5a5e8
lemon <1406428663@qq.com>
2017-03-13 22:55:35 +0800

    添加登录组件

---
+++ lemonmovie/src/components/login/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+    <h1>this is login</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less" scoped>
+.login {
+  width: 100%;
+  overflow: hidden;
+}
+</style>
